7fdad8381782e6b127417a0ace98fad929028a54
nikhil.shikarkhane <shikarkhane@gmail.com>
2014-03-18 10:59:04 +0000

    try catch for twitter as service

--- twitter_as_service.py
+++ twitter_as_service.py
@@ -1,5 +1,13 @@
 # creating an option to run fetcher as a service as it would be lighter than running tornado
 from twitter.tweet_talker import Tweet_talker
+import time
 
 stream = Tweet_talker()
-stream.statuses.filter(locations='-180,-90,180,90')
+
+while True:
+    try:
+        stream.statuses.filter(locations='-180,-90,180,90')
+    except Exception as e:
+        print str(e)
+        # sleep for a min before trying again
+        time.sleep(60)
