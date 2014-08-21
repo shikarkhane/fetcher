215089644a9796f6fad5730e28c06e89dcb8d128
nikhil.shikarkhane <shikarkhane@gmail.com>
2014-08-21 06:41:36 +0000

    instagram complaining - "too many calls per second"

--- insta_as_service.py
+++ insta_as_service.py
@@ -3,17 +3,22 @@ import settings
 from common.pipeline import Pipe
 from common import cache
 from dogpile.cache import make_region
+import time
 
 def insta_service(cache_coordinates, cache_post_ids):
     b = Base(settings.insta_access_token)
     # scan static coordinates
     try:
-        [b.write_igrams_to_file(c[0], c[1], cache_post_ids) for c in settings.insta_static_coordinates_to_scan]
+        for c in settings.insta_static_coordinates_to_scan:
+            b.write_igrams_to_file(c[0], c[1], cache_post_ids)
+            time.sleep(settings.insta_sleep_between_calls_in_sec)
     except Exception as e:
         print str(e)
     # scan dynamic coordinates stored in the cache
     try:
-        [b.write_igrams_to_file(c.split(',')[0], c.split(',')[1], cache_post_ids) for c in cache.get_all_keys(cache_coordinates)]
+        for c in cache.get_all_keys(cache_coordinates):
+            b.write_igrams_to_file(c.split(',')[0], c.split(',')[1], cache_post_ids)
+            time.sleep(settings.insta_sleep_between_calls_in_sec)
     except Exception as e:
         print str(e)
 
